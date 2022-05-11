572914d1705408f1166b362f4399d9a9c1548eca
Jokey Li <me@jokey.li>
2022-05-11 17:57:50 +0800

    update shell

--- run.sh
+++ run.sh
@@ -6,8 +6,8 @@ chmod +x build.sh && ./build.sh
 cd -
 
 # containerd runtime import
-docker save nextcloud/full > nextcloud-full.tar
-ctr -n=k8s.io images import nextcloud-full.tar && rm -f nextcloud-full.tar
+docker image save  nextcloud/full  > nextcloud_full.tar
+ctr -n=k8s.io images import apache_full/nextcloud_full.tar && rm -f nextcloud_full.tar
 
 # apply cus_configmap
 kubectl create configmap cus-php-config --from-file=./php_ini/cus-php-config.ini
